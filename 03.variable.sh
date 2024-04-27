395bc09e0bcdd706e20355f676ea0a9e0ec00e5f
Saswat <saswat@weinvestadmins-MacBook-Air.local>
2024-04-27 18:33:25 +0530

    Auto push

--- 03.variable.sh
+++ 03.variable.sh
@@ -7,3 +7,13 @@ c="scripting"
 echo "Value of a is $a"
 
 echo stop
+
+#DYNMIC VRIBLES
+
+TODAYS_DATE=(date +%F)
+
+echo Todays date is $TODAYS_DATE
+
+NUMBER_OF_USERS=(cat /etc/passwd | wc -l)
+
+echo Total number of registered users in this system is $NUMBER_OF_USERS
