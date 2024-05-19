453a6e426101a0dd1b7104a4c3cb8c894c53e268
Saswat <saswat@weinvestadmins-MacBook-Air.local>
2024-05-19 11:00:04 +0530

    Auto commit

---
+++ 05.command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# there are 4 types of commands available:
+
+# 1. Binary (/bin , /sbin)
+# 2. Aliases (Aliases are shortcuts i.e. alias net="netstat -tulpn")
+# 3. Shell built-in commands 
+# 4. Functions (Functions are nothing but a set of command that can be written in a sequence and can be called n number of times)
+
+#Decalring a function:
+sample() {
+    echo Decalred the function sample
+    echo We can call this function and these two lines will be executed
+    echo Hello executing sample
+}
+
+# calling the function
+
+sample
